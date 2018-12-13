3b46083bffc21cf775dd41536f90810e6bcafeb7
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2018-12-13 14:13:57 +0100

    Generator script
    Laver jorden

---
+++ OregonGame/Assets/Scripts/Generator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	
+	#pragma warning disable 78
+	
+	public GameObject Stone; 
+	public GameObject Dirt; 
+	
+	public int minX = -10;   //variable på længte og bredte
+	public int maxX = 10;
+	public int minY = -10;
+	public int maxY = 10;
+	
+	PerlinNoise noise;
+	
+	void Start () {
+		noise = new PerlinNoise(Random.Range(1000000,10000000));
+		Regenerate();
+	}
+	
+	private void Regenerate(){
+		
+		float width = Dirt.transform.lossyScale.x;
+		float height = Stone.transform.lossyScale.y;
+		
+		for (int i = minX; i < maxX; i++){//columns (x values)
+			int columnHeight = 2 + noise.getNoise(i - minX, maxY - minY - 2);
+			for(int j = minY; j < minY + columnHeight; j++){//rows (y values)
+				GameObject block = (j == minY + columnHeight - 1)?Dirt:Stone;
+				Instantiate(block, new Vector2(i * width, j * height), Quaternion.identity);
+			}
+		}
+	}
+}
