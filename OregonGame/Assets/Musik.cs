05d7dc1e7d270bc614e1b2ddb011df4d6c5d2dfb
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-01-17 11:13:57 +0100

    Færdig med musik
    added intro xD

---
+++ OregonGame/Assets/Musik.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musik : MonoBehaviour {
+
+    AudioSource Vanilla;
+    AudioSource Battle;
+
+    void Start()
+    {
+        Vanilla = GetComponent<AudioSource>();
+        Vanilla.Play(0);
+        Debug.Log("started");
+    }
+}
