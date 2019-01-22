7b15e1fd94ed2e36ecb44fdb7bfdaf3c17581362
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-01-22 11:23:11 +0100

    Kode
    Vil kode baggrund med i ground koden

--- OregonGame/Assets/Scripts/¨Ground/Generator.cs
+++ OregonGame/Assets/Scripts/¨Ground/Generator.cs
@@ -6,12 +6,15 @@ public class Generator : MonoBehaviour {
 	#pragma warning disable 78 //fjerner adversels kode 78
 	
 	public GameObject Stone; 
-	public GameObject Dirt; 
+	public GameObject Dirt;
+    public GameObject Back; //Baggrunden som er en sprite, renders som en character bagerst.
 	
 	public int minX = -10;   //variable på længte og bredte
 	public int maxX = 10;
 	public int minY = -10;
 	public int maxY = 10;
+    public int middleX = 5;
+    public int middley = 5;
     public float BlockSpace;
 	
 	PerlinNoise noise;
@@ -28,10 +31,12 @@ public class Generator : MonoBehaviour {
 		
 		for (int i = minX; i < maxX; i++){//columns (x values)
 			int columnHeight = 2 + noise.getNoise(i - minX, maxY - minY - 2);
+
 			for(int j = minY; j < minY + columnHeight; j++){//rows (y values)
-				GameObject block = (j == minY + columnHeight - 1)?Dirt:Stone;
+				GameObject block = (j == minY + columnHeight - 1)?Dirt:Stone; //fortæller hvilke block den skal spawne på hvilke lag
 				Instantiate(block, new Vector2(i * width / BlockSpace, j * height / BlockSpace), Quaternion.identity); //spawner blocke
 			}
 		}
+        for (int m = middleX; m < middleX
 	}
 }
