cc8c668677707d8cc5db7d61654e5824d7e99b56
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-01-25 08:33:21 +0100

    Block kode
    arbejder på block health, altså så vi kan ødelægge genstande minecraft style

---
+++ OregonGame/Assets/Scripts/¨Ground/BlockHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHealth : MonoBehaviour {
+
+    public int Health;
+
+    private void Update()
+    {
+        if(Health == 100)
+        {
+           
+        }
+        else if(Health == 80)
+        {
+
+        }
+        else if(Health == 60)
+        {
+
+        }
+        else if(Health == 40)
+        {
+
+        }
+        else if(Health == 20)
+        {
+
+        }
+        else if(Health == 0)
+        {
+            DestroyObject(gameObject);
+        }
+    }
+}
