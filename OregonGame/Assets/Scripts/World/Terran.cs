78416b765260be72b981350f1f21cd34567e974d
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-03-20 12:47:59 +0100

    Opsat til det nye system
    Fik rettet up på tiles, lyde, sprites.... og det

--- OregonGame/Assets/Scripts/World/Terran.cs
+++ OregonGame/Assets/Scripts/World/Terran.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Terran : MonoBehaviour
 {
+    public GameObject Tile;
+    public List<GameObject> Tiles;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tiles = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TileGenerate();
+    }
+
+    public void TileGenerate()
+    {
+        GameObject newtile = Instantiate(Tile, Vector2.zero, Quaternion.identity) as GameObject;
+        Tiles.Add(newtile);
     }
 }
