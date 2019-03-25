83df6b1769fb3636cbff7079a96093be6f79c7f2
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-03-25 12:38:44 +0100

    updates

--- OregonGame/Assets/Scripts/Objects/Items.cs
+++ OregonGame/Assets/Scripts/Objects/Items.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
+    public int Health = 1;
+    public int DropAmount = 1;
     // Start is called before the first frame update
     void Start()
     {
