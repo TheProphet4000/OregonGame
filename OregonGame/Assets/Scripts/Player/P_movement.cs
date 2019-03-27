0fcfe73c34c490b3f4cc8aae8c939a22e352302a
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-03-27 15:17:36 +0100

    Animation
    Har fået sat animation på figur... mangler lige små justeringer

--- OregonGame/Assets/Scripts/Player/P_movement.cs
+++ OregonGame/Assets/Scripts/Player/P_movement.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class P_movement : MonoBehaviour
 {
-    public Animator animator;
+    public Animator anim;
 
-
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
     void Update()
     {
+        //-bevægelse
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-
         transform.position = transform.position + movement * Time.deltaTime;
+
+        //-Amimation
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.Play("WalkUp");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.Play("WalkDown");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.Play("WalkLeft");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.Play("WalkRight");
+        }
+
     }
 }
