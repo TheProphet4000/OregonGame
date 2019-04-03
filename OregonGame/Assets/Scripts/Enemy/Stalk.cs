cde7903c91afc48ddfdf82150ec75cfa5d16b3e3
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-03 13:09:23 +0200

    Stalker (fejl men cool)
    Har lavet en fejl... men en cool fejl .. se mystiske mand i spil... han er et spøgelse

---
+++ OregonGame/Assets/Scripts/Enemy/Stalk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stalk : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 3f;
+    public float minrange = 10f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+    if(Vector2.Distance(transform.position, target.position) > 3)
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+}
---
+++ OregonGame/Assets/Scripts/Player/HealthAndDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthAndDeath : MonoBehaviour
+{
+    public int health = 100;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
