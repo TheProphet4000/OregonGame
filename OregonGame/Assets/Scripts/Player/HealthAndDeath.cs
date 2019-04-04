966a94ed2adcef1b557ab9b8b83691c1e004b9c2
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-04 10:42:59 +0200

    Selvmord
    Nu kan man begå selvmord.

--- OregonGame/Assets/Scripts/Player/HealthAndDeath.cs
+++ OregonGame/Assets/Scripts/Player/HealthAndDeath.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthAndDeath : MonoBehaviour
 {
     public int health = 100;
+
     void Start()
     {
         
     }
+    private void TakeDamage()
+    {
+            health = 0;
+    }
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.K))
+        {
+            Debug.Log("Key pressed");
+            TakeDamage();
+        }
+
+        if (health <= 0)
+        {
+            Debug.Log("Død");
+            Death();
+        }
+    }
+
+
+
+    private void Death()
+    {
+        SceneManager.LoadScene(2);
     }
 }
