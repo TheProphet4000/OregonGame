cd75c73a56a1ecb62ee4e94892ac7281eda01a62
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-10 12:00:21 +0200

    MADDD
    jeg har kodet mad funktioen færdigt... JAAAAAAAA NU KAN VI SPISE og dø af sult

--- OregonGame/Assets/Scripts/Player/Stomech.cs
+++ OregonGame/Assets/Scripts/Player/Stomech.cs
@@ -7,6 +7,7 @@ public class Stomech : MonoBehaviour
 {
 
     public int hunger = 20;
+    public int Mad = 10;
 
     void Start()
     {
@@ -16,6 +17,18 @@ public class Stomech : MonoBehaviour
     void Omnom()
     {
         hunger -= 10;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Mad")
+        {
+            hunger += Mad;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            DestroyObject(other.gameObject);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
     }
     private void Update()
     {
