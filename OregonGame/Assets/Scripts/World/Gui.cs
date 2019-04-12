73ee79ef1e50e9dacd779b3555d52962436d688b
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-12 13:11:05 +0200

    Menu
    Prøver af få menuer til at virke.... KAN IKKE FÅ DEt FUCKIng PISS TIL AT VIRKERFALLPSKFAP

--- OregonGame/Assets/Scripts/World/Gui.cs
+++ OregonGame/Assets/Scripts/World/Gui.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; //Need this for calling UI scripts
 
-public class Gui : MonoBehaviour
+public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
+
+    [SerializeField]
+    Text timeText; //Will assign our Time Text to this variable so we can modify the text it displays.
+
+    bool isPaused; //Used to determine paused state
+
     void Start()
     {
-        
+        UIPanel.gameObject.SetActive(false); //make sure our pause menu is disabled when scene starts
+        isPaused = false; //make sure isPaused is always false when our scene opens
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        timeText.text = "Time Since Startup: " + Time.timeSinceLevelLoad; //Tells us the time since the scene loaded
+
+        //If player presses escape and game is not paused. Pause game. If game is paused and player presses escape, unpause.
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+            Pause();
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+            UnPause();
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        UIPanel.gameObject.SetActive(true); //turn on the pause menu
+        Time.timeScale = 0f; //pause the game
+    }
+
+    public void UnPause()
+    {
+        isPaused = false;
+        UIPanel.gameObject.SetActive(false); //turn off pause menu
+        Time.timeScale = 1f; //resume game
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel("SpilVerden");
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 }
