1aac7e9063219c348a3b6f66ca09271c22e98832
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-22 13:30:07 +0200

    AlphaBuild
    Har fået menuerne til at virke.
    og nu er der faktisk et spil, man kan spille..... mangler lige hunger og health bar

--- OregonGame/Assets/Scripts/World/Menus.cs
+++ OregonGame/Assets/Scripts/World/Menus.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Manager : MonoBehaviour
+public class Menus : MonoBehaviour
 {
     public void QuitGame()
     {
@@ -13,12 +13,8 @@ public class Manager : MonoBehaviour
     public void Restart()
     {
 #pragma warning disable CS0618 // Type or member is obsolete
-        Application.LoadLevel("SpilVerden");
+        Application.LoadLevel(1);
 #pragma warning restore CS0618 // Type or member is obsolete
     }
-    void TaskOnClick()
-    {
-        //Output this to console when Button1 or Button3 is clicked
-        Debug.Log("You have clicked the button!");
-    }
+
 }
