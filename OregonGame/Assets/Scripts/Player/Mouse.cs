7188f833f94b144cdf22493590643c94c9a1f2c6
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-04-22 14:23:06 +0200

    Rettelse på alpha
    Fik ændret menuerne, og fikset UI der ikke var fixed til cameraret

---
+++ OregonGame/Assets/Scripts/Player/Mouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        //-fortæller at man skal kunne se musen på skærmen
+    }
+}
