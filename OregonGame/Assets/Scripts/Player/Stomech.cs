9e8ad70204c66e79559a67f890da0bcbae9bf639
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2019-05-02 15:02:23 +0200

    Nok den sidste ændring
    Har insat selvmordsfunktion, og rattet op på småting. Har skrevet en god sjat historie, og toturials (der skulle havdet været med i spillet)

--- OregonGame/Assets/Scripts/Player/Stomech.cs
+++ OregonGame/Assets/Scripts/Player/Stomech.cs
@@ -6,35 +6,39 @@ using UnityEngine.SceneManagement;
 public class Stomech : MonoBehaviour
 {
 
-    public int hunger = 20;
-    public int Mad = 10;
+    public int hunger = 100; //- hvormeget mad man har i forvejen
+    public int Mad = 10;  //- hvormeget mad, en madbox giver.
 
     void Start()
     {
         InvokeRepeating("Omnom", 0, 10.0f); //-a timer that repeats every 10 sekunds
     }
 
-    void Omnom()
+    void Omnom() //- denne funktion tager 10 fra hunger, hvert sekund.... så når hunger er 0, dør man
     {
         hunger -= 10;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // hvis man collidere med mad objected
     {
-        if (other.tag == "Mad")
+        if (other.tag == "Mad") // og mad objekt har tag "MAD"
         {
-            hunger += Mad;
+            hunger += Mad; // får man det mad variablen giver
 
 #pragma warning disable CS0618 // Type or member is obsolete
-            DestroyObject(other.gameObject);
+            DestroyObject(other.gameObject); // fjerner mad objekt når spist
 #pragma warning restore CS0618 // Type or member is obsolete
         }
     }
-    private void Update()
+    private void Update() // denne funktion tjekker for hvert cuklus af CPUen, hvad der sker... som den tjekker konstant om man har trykket K
     {
-        if (hunger <= 0)
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            Death();
+            Death(); //- hvis man trykker på K, dør man
+        }
+        if (hunger <= 0) // tjekker hvis man mangler hunger... hvis man gør dør man aka hvis hunger = 0, dør man
+        {
+            Death(); //refererre til death funktion
         }
     }
 
